bdc4c2cb65a70b72f732dbb1a48342b240b0aa96
Allan Oliveira <allanoliveira.main@gmail.com>
2016-02-24 13:21:10 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+/* server.js */
+
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// public folder to store assets
+app.use(express.static(__dirname + '/public'));
+
+// routes for app
+app.get('/', function(req, res) {
+  res.render('pad');
+});
+app.get('/(:id)', function(req, res) {
+  res.render('pad');
+});
+
+// get sharejs dependencies
+var sharejs = require('share');
+require('mongodb');
+
+// options for sharejs 
+var options = {
+  db: {type: 'mongo'},
+};
+
+// attach the express server to sharejs
+sharejs.server.attach(app, options);
+
+// listen on port 8000 (for localhost) or the port defined for heroku
+var port = process.env.PORT || 8000;
+app.listen(port);
