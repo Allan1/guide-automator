7ae82a88ccb482ed64b50126f464c02b814fe5ac
Allan Oliveira <allanoliveira.main@gmail.com>
2016-02-28 11:06:40 -0300

    capture code from markdown input

--- server.js
+++ server.js
@@ -29,6 +29,27 @@ var options = {
 // attach the express server to sharejs
 sharejs.server.attach(app, options);
 
+// webdriver
+// var fs = require('fs');
+// var webdriver = require('selenium-webdriver'),
+//     By = require('selenium-webdriver').By,
+//     until = require('selenium-webdriver').until;
+
+// var driver = new webdriver.Builder()
+//     .forBrowser('firefox')
+//     .build();
+
+// driver.get('http://www.google.com/ncr');
+// driver.findElement(By.name('q')).sendKeys('webdriver');
+// driver.findElement(By.name('btnG')).click();
+// driver.wait(until.titleIs('webdriver - Google Search'), 4000);
+// driver.takeScreenshot().then(function(data) {
+//   name = 'ss.png';
+//   var screenshotPath = 'public/';
+//   fs.writeFileSync(screenshotPath + name, data, 'base64');
+// });
+// driver.quit();
+
 // listen on port 8000 (for localhost) or the port defined for heroku
 var port = process.env.PORT || 8000;
 app.listen(port);
