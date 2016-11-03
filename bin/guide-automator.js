42f1a41fc339c7dd0b85bf6dddfa4ff7e68d3056
Welbert Serra <welberts@gmail.com>
2016-11-02 21:28:20 -0300

    Exports de função no app

--- bin/guide-automator.js
+++ bin/guide-automator.js
@@ -1,19 +1,26 @@
-var imgCount = 0;
-var options = {
-	output: ""
-};
 var fs = require("fs");
-
+var gm = require('gm').subClass({
+	imageMagick: true
+});
 var webdriver = require('selenium-webdriver'),
 	By = require('selenium-webdriver').By,
 	until = require('selenium-webdriver').until;
-
 var driver = new webdriver.Builder()
 	.forBrowser('chrome')
 	.build();
 
+var imgCount = 0;
+
+
+var options = {
+	output: "",
+	outlineStyle: "solid red 3px"
+};
+
+
 exports.defineOptions = function(arg) {
-	options = arg;
+	options.output = arg.output;
+	options.outlineStyle = arg.outlineStyle;
 };
 
 /**
@@ -25,14 +32,13 @@ exports.get = function(url) {
 	driver.get(url);
 };
 
-exports.takeScreenshot = function(cssSelector) {
-	//var width = DEFAULT_IMG_WIDTH;
-	/*if (cmd.params.length > 1) {
-		width = cmd.params[1];
-	}*/
-
-	//replaceBlockWithImage(cmd.blockIndex, imgCount + '.png', width);
+/**
+ * Tira foto da parte visivel do site acessado
+ * @return {string}   Nome da imagem gerada
+ */
+exports.takeScreenshot = function() {
 	imgCount++;
+
 	driver.takeScreenshot().then(
 		function(image, err) {
 			if (err) {
@@ -47,4 +53,103 @@ exports.takeScreenshot = function(cssSelector) {
 		}
 	);
 	return imgCount + '.png';
+};
+
+/**
+ * Tira foto do cssSelector passado, podendo dar crop ou highlight
+ * @param  {string} cssSelector Css selector do DOM
+ * @param  {int} crop        [Opicional] 1 - crop ativo, 0 - crop inativo
+ * @param  {int} outline     [Opicional] 1 - outline ativo, 0 - outline inativo
+ * @return {string}             Nome da imagem gerada
+ */
+exports.takeScreenshotOf = function(cssSelector, crop, outline) {
+	imgCount++;
+
+	driver.findElement(By.css(cssSelector)).then(function(el) {
+		if (outline) {
+			driver.executeScript("arguments[0].style.outline = '" + options.outlineStyle + "'", el);
+		}
+		driver.executeScript("return arguments[0].getBoundingClientRect()", el).then(function(rect) {
+			driver.takeScreenshot().then(
+				function(image, err) {
+					driver.executeScript("arguments[0].style.outline = ''", el);
+					if (crop) {
+						var img = new Buffer(image, 'base64');
+						gm(img)
+							.crop(rect.width, rect.height, rect.left, rect.top)
+							.write(options.output + '/' + imgCount + '.png', function(err) {
+								if (err) {
+									throw err;
+								}
+							});
+					} else {
+						fs.writeFile(options.output + '/' + imgCount + '.png', image, 'base64', function(err) {
+							if (err) {
+								throw err;
+							}
+						});
+					}
+				}
+			);
+		});
+	});
+	return imgCount + '.png';
+};
+
+/**
+ * Preenche infomações de um determinado campo
+ * @param  {string} cssSelector css selector do DOM desejado
+ * @param  {string} text        Texto que será escrito no campo
+ * @return {none}
+ */
+exports.fillIn = function(cssSelector, text) {
+	driver.findElement(By.css(cssSelector)).clear();
+	driver.findElement(By.css(cssSelector)).sendKeys(text);
+};
+
+/**
+ * Faz o submit no cssSelector
+ * @param  {string} cssSelector css Selector do DOM desejado
+ * @return {none}
+ */
+exports.submit = function(cssSelector) {
+	driver.findElement(By.css(cssSelector)).submit();
+};
+
+/**
+ * Clica em um determinado DOM
+ * @param  {string} cssSelector css Selector do DOM desejado
+ * @return {none}
+ */
+exports.click = function(cssSelector) {
+	driver.findElement(By.css(cmds[i].params[0])).click();
+};
+
+/**
+ * Clica em um determinado elemento utilizando o nome do linkText
+ * @param  {string} text Texto a ser buscado nos links da interface
+ * @return {none}
+ */
+exports.clickByLinkText = function(text) {
+	driver.findElement(By.linkText(text)).click();
+};
+
+/**
+ * Faz o driver parar por um tempo solicitado
+ * @param  {int} sleepTime tempo em milisegundos para sleep
+ * @return {none}
+ */
+exports.sleep = function(sleepTime) {
+	driver.sleep(parseInt(sleepTime));
+};
+
+/**
+ * Aguarda até um elemento da tela carregar ou até o limite de tempo
+ * @param  {string} cssSelector css Selector do DOM desejado
+ * @param  {int} timeOut     [Opicional]Tempo em milisegundo, default = 5000
+ * @return {none}
+ */
+exports.wait = function(cssSelector, timeOut) {
+	var timeLimit = timeOut || 5000;
+	driver.wait(until.elementLocated(By.css(cssSelector)), timeLimit);
 };
--- server.js
+++ server.js
@@ -4,15 +4,10 @@
 //TODO olhar os outros TO DO
 
 //--Variaveis ------------------
+var DEFAULT_IMG_WIDTH = '60%';
 var fs = require("fs");
-var options = {
-	output: ""
-};
 var path = require("path");
 var showdown = require('showdown');
-var gm = require('gm').subClass({
-	imageMagick: true
-});
 var wkhtmltopdf = require('wkhtmltopdf');
 var input, output;
 var outputHTMLFile = 'manual.html';
@@ -29,18 +24,14 @@ var cmdsDictionary = [
 	'sleep',
 	'clickByLinkText'
 ];
-var imgCount = 0;
-var imgCountSel = 0;
 var seleniumBlocks = [];
 var markdownText = '';
-var DEFAULT_IMG_WIDTH = '60%';
 var converter = new showdown.Converter({
 	parseImgDimensions: true
 });
 var html_start = '<!DOCTYPE html><html lang="en"><head><title></title><meta charset="UTF-8"></head><body>';
 var html_end = '</body></html>';
 var html_pagebreak = '<div style="page-break-after: always;"></div>';
-var outline_style = 'solid red 3px';
 var wkhtmltopdf_options = {
 	pageSize: 'letter',
 	output: null,
@@ -83,7 +74,7 @@ if (!process.argv[3]) {
 	process.exit();
 }
 output = process.argv[3];
-wkhtmltopdf_options.output = process.argv[3] + '/' + outputPDFFile;
+wkhtmltopdf_options.output = output + '/' + outputPDFFile;
 
 guideAutomator.defineOptions({
 	output: output
@@ -182,13 +173,21 @@ function execSelenium(seleniumBlocks, cb) {
 						break;
 					case 'takeScreenshot':
 						width = cmds[i].params[0] || DEFAULT_IMG_WIDTH;
-						screenShotName = guideAutomator.takeScreenshot(cmds[i]);
+						screenShotName = guideAutomator.takeScreenshot();
 						replaceBlockWithImage(cmds[i].blockIndex,
 							screenShotName,
 							width);
 						break;
 					case 'takeScreenshotOf':
-						takeScreenshotOf(cmds[i], cb);
+						width = cmds[i].params[3] || DEFAULT_IMG_WIDTH;
+						screenShotName = guideAutomator.takeScreenshotOf(
+							cmds[i].params[0],
+							cmds[i].params[1],
+							cmds[i].params[2]
+						);
+						replaceBlockWithImage(cmds[i].blockIndex,
+							screenShotName,
+							width);
 						break;
 					case 'scrollTo':
 						// driver.findElement(By.css(cmds[i].params[0])).then(function(el) {
@@ -201,17 +200,16 @@ function execSelenium(seleniumBlocks, cb) {
 						// var text = driver.findElement(By.css(cmds[i].params[0])).getText();
 						break;
 					case 'fillIn':
-						driver.findElement(By.css(cmds[i].params[0])).clear();
-						driver.findElement(By.css(cmds[i].params[0])).sendKeys(cmds[i].params[1]);
+						guideAutomator.fillIn(cmds[i].params[0], cmds[i].params[1]);
 						break;
 					case 'submit':
-						driver.findElement(By.css(cmds[i].params[0])).submit();
+						guideAutomator.submit(cmds[i].params[0]);
 						break;
 					case 'click':
-						driver.findElement(By.css(cmds[i].params[0])).click();
+						guideAutomator.click(cmds[i].params[0]);
 						break;
 					case 'clickByLinkText':
-						driver.findElement(By.linkText(cmds[i].params[0])).click();
+						guideAutomator.clickByLinkText(cmds[i].params[0]);
 						break;
 					case 'hover':
 						// driver.findElement(By.css(cmds[i].params[0])).then(function(elem){
@@ -220,16 +218,10 @@ function execSelenium(seleniumBlocks, cb) {
 						// });
 						break;
 					case 'sleep':
-						driver.sleep(parseInt(cmds[i].params[0]));
+						guideAutomator.sleep(cmds[i].params[0]);
 						break;
 					case 'wait':
-						var cmd = cmds[i];
-						if (cmd.params.length > 1) {
-							timeout = cmd.params[1];
-						} else {
-							timeout = 5000;
-						}
-						driver.wait(until.elementLocated(By.css(cmd.params[0])), timeout);
+						guideAutomator.wait(cmds[i].params[0], cmds[i].params[1]);
 						break;
 					default:
 						break;
@@ -249,86 +241,6 @@ function replaceBlockWithImage(blockIndex, filename, width) {
 			return '<replaceSelenium>![](' + filename + ' =' + width + 'x*)';
 		}
 		return match;
-	});
-}
-
-function takeScreenshot(cmd, cb) {
-	//imgCount++;
-	var width = DEFAULT_IMG_WIDTH;
-	if (cmd.params.length > 1) {
-		width = cmd.params[1];
-	}
-
-	replaceBlockWithImage(cmd.blockIndex, imgCount + '.png', width);
-
-	driver.takeScreenshot().then(
-		function(image, err) {
-			imgCountSel++;
-			if (err) {
-				return cb(err);
-			} else {
-				fs.writeFile(output + '/' + imgCountSel + '.png', image, 'base64', function(err) {
-					if (err) {
-						return cb(err);
-					}
-				});
-			}
-		}
-	);
-}
-
-function takeScreenshotOf(cmd, cb) {
-	imgCount++;
-	var width = DEFAULT_IMG_WIDTH;
-	var crop = false;
-	var outline = false;
-	var cssSelector = null;
-	if (!cmd.params.length) {
-		console.log("Missing param for takeScreenshotOf");
-		return cb(null);
-	} else {
-		cssSelector = cmd.params[0];
-	}
-
-	if (cmd.params.length > 1) {
-		crop = parseInt(cmd.params[1]);
-	}
-	if (cmd.params.length > 2) {
-		outline = parseInt(cmd.params[2]);
-	}
-	if (cmd.params.length > 3) {
-		width = cmd.params[3];
-	}
-
-	replaceBlockWithImage(cmd.blockIndex, imgCount + '.png', width);
-	driver.findElement(By.css(cssSelector)).then(function(el) {
-		if (outline) {
-			driver.executeScript("arguments[0].style.outline = '" + outline_style + "'", el);
-		}
-		driver.executeScript("return arguments[0].getBoundingClientRect()", el).then(function(rect) {
-			driver.takeScreenshot().then(
-				function(image, err) {
-					driver.executeScript("arguments[0].style.outline = ''", el);
-					imgCountSel++;
-					if (crop) {
-						var img = new Buffer(image, 'base64');
-						gm(img)
-							.crop(rect.width, rect.height, rect.left, rect.top)
-							.write(output + '/' + imgCountSel + '.png', function(err) {
-								if (err) {
-									return cb(err);
-								}
-							});
-					} else {
-						fs.writeFile(output + '/' + imgCountSel + '.png', image, 'base64', function(err) {
-							if (err) {
-								return cb(err);
-							}
-						});
-					}
-				}
-			);
-		});
 	});
 }
 
