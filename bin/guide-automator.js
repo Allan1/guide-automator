4165ee421363e422e2918076c8534276dc04afd2
Welbert Serra <welberts@gmail.com>
2016-11-02 22:41:06 -0300

    Passado o compilador e suas funções para um modulo separado

--- bin/guide-automator.js
+++ bin/guide-automator.js
@@ -10,33 +10,62 @@ var driver = new webdriver.Builder()
 	.build();
 
 var imgCount = 0;
-
+var DEFAULT_IMG_WIDTH = '60%';
+var seleniumBlocks = [];
+var markdownText = '';
+var html_pagebreak = '<div style="page-break-after: always;"></div>';
 
 var options = {
 	output: "",
 	outlineStyle: "solid red 3px"
 };
+var cmdsDictionary = [
+	'get',
+	'click',
+	'takeScreenshot',
+	'scrollTo',
+	'takeScreenshotOf',
+	'fillIn',
+	'submit',
+	'wait',
+	'sleep',
+	'clickByLinkText'
+];
+
+module.exports = {
+	defineOptions: defineOptions,
+	get: get,
+	takeScreenshot: takeScreenshot,
+	takeScreenshotOf: takeScreenshotOf,
+	fillIn: fillIn,
+	submit: submit,
+	click: click,
+	clickByLinkText: clickByLinkText,
+	sleep: sleep,
+	wait: wait,
+	guideAutomatorParse: guideAutomatorParse
+};
 
 
-exports.defineOptions = function(arg) {
+function defineOptions(arg) {
 	options.output = arg.output;
 	options.outlineStyle = arg.outlineStyle;
-};
+}
 
 /**
  * Acessa o site
  * @param  {string} url Site a ser acessado
  * @return {none}
  */
-exports.get = function(url) {
+function get(url) {
 	driver.get(url);
-};
+}
 
 /**
  * Tira foto da parte visivel do site acessado
  * @return {string}   Nome da imagem gerada
  */
-exports.takeScreenshot = function() {
+function takeScreenshot() {
 	imgCount++;
 
 	driver.takeScreenshot().then(
@@ -53,7 +82,7 @@ exports.takeScreenshot = function() {
 		}
 	);
 	return imgCount + '.png';
-};
+}
 
 /**
  * Tira foto do cssSelector passado, podendo dar crop ou highlight
@@ -62,18 +91,18 @@ exports.takeScreenshot = function() {
  * @param  {int} outline     [Opicional] 1 - outline ativo, 0 - outline inativo
  * @return {string}             Nome da imagem gerada
  */
-exports.takeScreenshotOf = function(cssSelector, crop, outline) {
+function takeScreenshotOf(cssSelector, crop, outline) {
 	imgCount++;
 
 	driver.findElement(By.css(cssSelector)).then(function(el) {
-		if (outline) {
+		if (parseInt(outline)) {
 			driver.executeScript("arguments[0].style.outline = '" + options.outlineStyle + "'", el);
 		}
 		driver.executeScript("return arguments[0].getBoundingClientRect()", el).then(function(rect) {
 			driver.takeScreenshot().then(
 				function(image, err) {
-					driver.executeScript("arguments[0].style.outline = ''", el);
-					if (crop) {
+					//driver.executeScript("arguments[0].style.outline = ''", el);
+					if (parseInt(crop)) {
 						var img = new Buffer(image, 'base64');
 						gm(img)
 							.crop(rect.width, rect.height, rect.left, rect.top)
@@ -94,7 +123,7 @@ exports.takeScreenshotOf = function(cssSelector, crop, outline) {
 		});
 	});
 	return imgCount + '.png';
-};
+}
 
 /**
  * Preenche infomações de um determinado campo
@@ -102,46 +131,46 @@ exports.takeScreenshotOf = function(cssSelector, crop, outline) {
  * @param  {string} text        Texto que será escrito no campo
  * @return {none}
  */
-exports.fillIn = function(cssSelector, text) {
+function fillIn(cssSelector, text) {
 	driver.findElement(By.css(cssSelector)).clear();
 	driver.findElement(By.css(cssSelector)).sendKeys(text);
-};
+}
 
 /**
  * Faz o submit no cssSelector
  * @param  {string} cssSelector css Selector do DOM desejado
  * @return {none}
  */
-exports.submit = function(cssSelector) {
+function submit(cssSelector) {
 	driver.findElement(By.css(cssSelector)).submit();
-};
+}
 
 /**
  * Clica em um determinado DOM
  * @param  {string} cssSelector css Selector do DOM desejado
  * @return {none}
  */
-exports.click = function(cssSelector) {
+function click(cssSelector) {
 	driver.findElement(By.css(cmds[i].params[0])).click();
-};
+}
 
 /**
  * Clica em um determinado elemento utilizando o nome do linkText
  * @param  {string} text Texto a ser buscado nos links da interface
  * @return {none}
  */
-exports.clickByLinkText = function(text) {
+function clickByLinkText(text) {
 	driver.findElement(By.linkText(text)).click();
-};
+}
 
 /**
  * Faz o driver parar por um tempo solicitado
  * @param  {int} sleepTime tempo em milisegundos para sleep
  * @return {none}
  */
-exports.sleep = function(sleepTime) {
+function sleep(sleepTime) {
 	driver.sleep(parseInt(sleepTime));
-};
+}
 
 /**
  * Aguarda até um elemento da tela carregar ou até o limite de tempo
@@ -149,7 +178,148 @@ exports.sleep = function(sleepTime) {
  * @param  {int} timeOut     [Opicional]Tempo em milisegundo, default = 5000
  * @return {none}
  */
-exports.wait = function(cssSelector, timeOut) {
+function wait(cssSelector, timeOut) {
 	var timeLimit = timeOut || 5000;
 	driver.wait(until.elementLocated(By.css(cssSelector)), timeLimit);
-};
+}
+
+function replaceBlockWithImage(blockIndex, filename, width) {
+	var index = 0;
+	markdownText = markdownText.replace(/<replaceSelenium>/g, function(match) {
+		index++;
+		if ((index - 1) === blockIndex) {
+			return '<replaceSelenium>![](' + filename + ' =' + width + 'x*)';
+		}
+		return match;
+	});
+}
+
+function extractMarkdownAndSelenium(markdownAndCode, cb) {
+	var rePattern = /<automator>([\s\S]+?)<\/automator>/g;
+	markdownText = markdownAndCode.replace(rePattern, function(match, p1, offset, string) {
+		p1 = p1.replace(/\r?\n|\r/g, ''); //TODO Verificar se isso que deu erro ao dar espaço/tab antes de chamar a função
+		seleniumBlocks.push(p1);
+		return '<replaceSelenium>';
+	});
+	return cb(seleniumBlocks);
+}
+
+function compile(seleniumBlocks, cb) {
+	var cmds = [];
+	var j = 0;
+	for (var i = 0; i < seleniumBlocks.length; i++) {
+		var tokens = seleniumBlocks[i].split(';');
+		for (var o = 0; o < tokens.length; o++) {
+			if (tokens[o] !== null && tokens[o] !== '') {
+				var matches = tokens[o].match(/^(\w+)(?:\(((?:\'.+\'|\d+)(?:,(?:\'.+\'|\d+))*)\))?$/);
+				if (matches && (cmdsDictionary.indexOf(matches[1]) > -1)) { // IE9
+					cmds[j] = {};
+					cmds[j].cmd = matches[1];
+					cmds[j].params = matches[2] ? matches[2].replace(/["']/g, "").split(',') : [];
+					cmds[j].blockIndex = i;
+					j++;
+				} else {
+					return cb("Invalid token: " + tokens[o]);
+				}
+			}
+		}
+	}
+	return cb(null, cmds);
+}
+
+//-- Fim Tratamento dos blocos 'automator'-----
+
+//-- Tratamento dos tokens ou execução de funcionalidades 'automator'  ------
+//TODO Isolar a lógica de cada funcionalidade e chamar pelo require
+function execSelenium(seleniumBlocks, cb) {
+	var result;
+	return compile(seleniumBlocks, function(err, cmds) {
+		if (err) {
+			return cb(err);
+		} else {
+			for (var i = 0; i < cmds.length; i++) {
+				switch (cmds[i].cmd) {
+					case 'get':
+						get(cmds[i].params[0]);
+						break;
+					case 'takeScreenshot':
+						width = cmds[i].params[0] || DEFAULT_IMG_WIDTH;
+						screenShotName = takeScreenshot();
+						replaceBlockWithImage(cmds[i].blockIndex,
+							screenShotName,
+							width);
+						break;
+					case 'takeScreenshotOf':
+						width = cmds[i].params[3] || DEFAULT_IMG_WIDTH;
+						screenShotName = takeScreenshotOf(
+							cmds[i].params[0],
+							cmds[i].params[1],
+							cmds[i].params[2]
+						);
+						replaceBlockWithImage(cmds[i].blockIndex,
+							screenShotName,
+							width);
+						break;
+					case 'scrollTo':
+						// driver.findElement(By.css(cmds[i].params[0])).then(function(el) {
+						// 	el.getLocation().then(function (position) {
+						// 		driver.touchActions().scroll({x:position.x,y:position.y}).perform();
+						// 	})
+						// 	});
+						break;
+					case 'getText':
+						// var text = driver.findElement(By.css(cmds[i].params[0])).getText();
+						break;
+					case 'fillIn':
+						fillIn(cmds[i].params[0], cmds[i].params[1]);
+						break;
+					case 'submit':
+						submit(cmds[i].params[0]);
+						break;
+					case 'click':
+						click(cmds[i].params[0]);
+						break;
+					case 'clickByLinkText':
+						clickByLinkText(cmds[i].params[0]);
+						break;
+					case 'hover':
+						// driver.findElement(By.css(cmds[i].params[0])).then(function(elem){
+						// 	driver.actions().mouseMove(elem).perform();
+						// 	driver.sleep(cmds[i].params[1]);
+						// });
+						break;
+					case 'sleep':
+						sleep(cmds[i].params[0]);
+						break;
+					case 'wait':
+						wait(cmds[i].params[0], cmds[i].params[1]);
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		return cb(null);
+	});
+}
+
+function replaceRemainingBlocks(cb) {
+	markdownText = markdownText.replace(/<replaceSelenium>/g, "").replace(/\\pagebreak/g, html_pagebreak);
+	return cb(markdownText);
+}
+
+function guideAutomatorParse(data, cb) {
+	return extractMarkdownAndSelenium(data, function(seleniumBlocks) { //Extração dos blocos 'automator'
+		return execSelenium(seleniumBlocks, function(err) { //Execução e tratamento dos tokens dos blocos 'automator'
+			if (err) {
+				return cb(null, err);
+			} else {
+				driver.quit().then(function() {
+					return replaceRemainingBlocks(cb);
+					//clearAndExportFiles(cb); //Limpa os '<replaceSelenium>' e exporta para pdf e html.
+					//TODO Permitir escolher o que exportar, mas por default (pdf e html)
+				});
+			}
+		});
+	});
+}
--- server.js
+++ server.js
@@ -31,7 +31,6 @@ var converter = new showdown.Converter({
 });
 var html_start = '<!DOCTYPE html><html lang="en"><head><title></title><meta charset="UTF-8"></head><body>';
 var html_end = '</body></html>';
-var html_pagebreak = '<div style="page-break-after: always;"></div>';
 var wkhtmltopdf_options = {
 	pageSize: 'letter',
 	output: null,
@@ -108,161 +107,21 @@ function processInput(input, cb) {
 		if (err) {
 			return cb(err);
 		}
-		return extractMarkdownAndSelenium(data, function(seleniumBlocks) { //Extração dos blocos 'automator'
-			return execSelenium(seleniumBlocks, function(err) { //Execução e tratamento dos tokens dos blocos 'automator'
-				if (err) {
-					return cb(err);
-				} else {
-					driver.quit().then(function() {
-						clearAndExportFiles(cb); //Limpa os '<replaceSelenium>' e exporta para pdf e html.
-						//TODO Permitir escolher o que exportar, mas por default (pdf e html)
-					});
-				}
-			});
+		return guideAutomator.guideAutomatorParse(data, function(value, err) {
+			return exportFiles(value, cb); //Limpa os '<replaceSelenium>' e exporta para pdf e html.
+			//TODO Permitir escolher o que exportar, mas por default (pdf e html)
 		});
 	});
 }
-
-function extractMarkdownAndSelenium(markdownAndCode, cb) {
-	var rePattern = /<automator>([\s\S]+?)<\/automator>/g;
-	markdownText = markdownAndCode.replace(rePattern, function(match, p1, offset, string) {
-		p1 = p1.replace(/\r?\n|\r/g, ''); //TODO Verificar se isso que deu erro ao dar espaço/tab antes de chamar a função
-		seleniumBlocks.push(p1);
-		return '<replaceSelenium>';
-	});
-	return cb(seleniumBlocks);
-}
-
-function compile(seleniumBlocks, cb) {
-	var cmds = [];
-	var j = 0;
-	for (var i = 0; i < seleniumBlocks.length; i++) {
-		var tokens = seleniumBlocks[i].split(';');
-		for (var o = 0; o < tokens.length; o++) {
-			if (tokens[o] !== null && tokens[o] !== '') {
-				var matches = tokens[o].match(/^(\w+)(?:\(((?:\'.+\'|\d+)(?:,(?:\'.+\'|\d+))*)\))?$/);
-				if (matches && (cmdsDictionary.indexOf(matches[1]) > -1)) { // IE9
-					cmds[j] = {};
-					cmds[j].cmd = matches[1];
-					cmds[j].params = matches[2] ? matches[2].replace(/["']/g, "").split(',') : [];
-					cmds[j].blockIndex = i;
-					j++;
-				} else {
-					return cb("Invalid token: " + tokens[o]);
-				}
-			}
-		}
-	}
-	return cb(null, cmds);
-}
-
-//-- Fim Tratamento dos blocos 'automator'-----
-
-//-- Tratamento dos tokens ou execução de funcionalidades 'automator'  ------
-//TODO Isolar a lógica de cada funcionalidade e chamar pelo require
-function execSelenium(seleniumBlocks, cb) {
-	var result;
-	return compile(seleniumBlocks, function(err, cmds) {
-		if (err) {
-			return cb(err);
-		} else {
-			for (var i = 0; i < cmds.length; i++) {
-				switch (cmds[i].cmd) {
-					case 'get':
-						guideAutomator.get(cmds[i].params[0]);
-						break;
-					case 'takeScreenshot':
-						width = cmds[i].params[0] || DEFAULT_IMG_WIDTH;
-						screenShotName = guideAutomator.takeScreenshot();
-						replaceBlockWithImage(cmds[i].blockIndex,
-							screenShotName,
-							width);
-						break;
-					case 'takeScreenshotOf':
-						width = cmds[i].params[3] || DEFAULT_IMG_WIDTH;
-						screenShotName = guideAutomator.takeScreenshotOf(
-							cmds[i].params[0],
-							cmds[i].params[1],
-							cmds[i].params[2]
-						);
-						replaceBlockWithImage(cmds[i].blockIndex,
-							screenShotName,
-							width);
-						break;
-					case 'scrollTo':
-						// driver.findElement(By.css(cmds[i].params[0])).then(function(el) {
-						// 	el.getLocation().then(function (position) {
-						// 		driver.touchActions().scroll({x:position.x,y:position.y}).perform();
-						// 	})
-						// 	});
-						break;
-					case 'getText':
-						// var text = driver.findElement(By.css(cmds[i].params[0])).getText();
-						break;
-					case 'fillIn':
-						guideAutomator.fillIn(cmds[i].params[0], cmds[i].params[1]);
-						break;
-					case 'submit':
-						guideAutomator.submit(cmds[i].params[0]);
-						break;
-					case 'click':
-						guideAutomator.click(cmds[i].params[0]);
-						break;
-					case 'clickByLinkText':
-						guideAutomator.clickByLinkText(cmds[i].params[0]);
-						break;
-					case 'hover':
-						// driver.findElement(By.css(cmds[i].params[0])).then(function(elem){
-						// 	driver.actions().mouseMove(elem).perform();
-						// 	driver.sleep(cmds[i].params[1]);
-						// });
-						break;
-					case 'sleep':
-						guideAutomator.sleep(cmds[i].params[0]);
-						break;
-					case 'wait':
-						guideAutomator.wait(cmds[i].params[0], cmds[i].params[1]);
-						break;
-					default:
-						break;
-				}
-			}
-		}
-		return cb(null);
-	});
-}
-
-
-function replaceBlockWithImage(blockIndex, filename, width) {
-	var index = 0;
-	markdownText = markdownText.replace(/<replaceSelenium>/g, function(match) {
-		index++;
-		if ((index - 1) === blockIndex) {
-			return '<replaceSelenium>![](' + filename + ' =' + width + 'x*)';
-		}
-		return match;
-	});
-}
-
+/*
 function replaceRemainingBlocks(cb) {
 	markdownText = markdownText.replace(/<replaceSelenium>/g, "").replace(/\\pagebreak/g, html_pagebreak);
 	return cb(null, markdownText);
 }
-
+*/
 //-- Fim Tratamento dos tokens ou execução de funcionalidades 'automator' ------
 
 //-- Tratamento para exportar o produto final ----------
-//TODO Isolar a lógica de cada funcionalidade e chamar pelo require
-function clearAndExportFiles(cb) {
-	replaceRemainingBlocks(function(err, outputMarkdown) {
-		if (err) {
-			return cb(err);
-		} else {
-			return exportFiles(outputMarkdown, cb);
-		}
-	});
-}
-
 function exportFiles(text, cb) {
 	var html = html_start + converter.makeHtml(text) + html_end;
 	fs.writeFileSync(output + '/' + outputHTMLFile, html);
