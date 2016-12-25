30d5e1b178394fdf7a2f1cb8dfbec417b53246de
Welbert Serra <welberts@gmail.com>
2016-12-25 12:53:08 -0300

    Legacy mode is dead

--- bin/guide-automator-parser.js
+++ bin/guide-automator-parser.js
@@ -1,7 +1,6 @@
 var options = {
 	output: "",
 	outlineStyle: "solid red 3px",
-	legacy: false,
 	debug: false
 };
 
@@ -12,23 +11,9 @@ module.exports = {
 
 var guideAutomator = require('./guide-automator-function');
 
-var DEFAULT_IMG_WIDTH = '60%';
 var seleniumBlocks = [];
 var markdownText = '';
 var html_pagebreak = '<div style="page-break-after: always;"></div>';
-
-var cmdsDictionary = [
-	'get',
-	'click',
-	'takeScreenshot',
-	'scrollTo',
-	'takeScreenshotOf',
-	'fillIn',
-	'submit',
-	'wait',
-	'sleep',
-	'clickByLinkText'
-];
 
 function defineOptions(arg) {
 	Object.keys(options).forEach(function(key) {
@@ -36,124 +21,7 @@ function defineOptions(arg) {
 	});
 }
 
-function replaceBlockWithImage(blockIndex, filename, width) {
-	var index = 0;
-	markdownText = markdownText.replace(/<replaceSelenium>/g, function(match) {
-		index++;
-		if ((index - 1) === blockIndex) {
-			return '<replaceSelenium>![](' + filename + ' =' + width + 'x*)';
-		}
-		return match;
-	});
-}
-
-function extractMarkdownAndSelenium(markdownAndCode, cb) {
-	var rePattern = /<automator>([\s\S]+?)<\/automator>/g;
-	markdownText = markdownAndCode.replace(rePattern, function(match, p1, offset, string) {
-		p1 = p1.replace(/\r?\n|\r/g, '');
-		seleniumBlocks.push(p1);
-		return '<replaceSelenium>';
-	});
-	return cb(seleniumBlocks);
-}
-
-function compile(seleniumBlocks, cb) {
-	var cmds = [];
-	var j = 0;
-	for (var i = 0; i < seleniumBlocks.length; i++) {
-		var tokens = seleniumBlocks[i].split(';');
-		for (var o = 0; o < tokens.length; o++) {
-			if (tokens[o] !== null && tokens[o] !== '') {
-				var matches = tokens[o].trim().match(/^(\w+)(?:\(((?:\'.+\'|\d+)(?:,(?:\'.+\'|\d+))*)\))?$/);
-				if (matches && (cmdsDictionary.indexOf(matches[1]) > -1)) { // IE9
-					cmds[j] = {};
-					cmds[j].cmd = matches[1];
-					cmds[j].params = matches[2] ? matches[2].replace(/["']/g, "").split(',') : [];
-					cmds[j].blockIndex = i;
-					j++;
-				} else {
-					return cb("Invalid token: " + tokens[o]);
-				}
-			}
-		}
-	}
-	return cb(null, cmds);
-}
-
 //-- Fim Tratamento dos blocos 'automator'-----
-
-//-- Tratamento dos tokens ou execução de funcionalidades 'automator'  ------
-function execSelenium(seleniumBlocks, cb) {
-	var result;
-	return compile(seleniumBlocks, function(err, cmds) {
-		if (err) {
-			return cb(err);
-		} else {
-			for (var i = 0; i < cmds.length; i++) {
-				switch (cmds[i].cmd) {
-					case 'get':
-						guideAutomator.get(cmds[i].params[0]);
-						break;
-					case 'takeScreenshot':
-						width = cmds[i].params[0] || DEFAULT_IMG_WIDTH;
-						screenShotName = guideAutomator.takeScreenshot();
-						replaceBlockWithImage(cmds[i].blockIndex,
-							screenShotName,
-							width);
-						break;
-					case 'takeScreenshotOf':
-						width = cmds[i].params[3] || DEFAULT_IMG_WIDTH;
-						screenShotName = guideAutomator.takeScreenshotOf(
-							cmds[i].params[0],
-							cmds[i].params[1],
-							cmds[i].params[2]
-						);
-						replaceBlockWithImage(cmds[i].blockIndex,
-							screenShotName,
-							width);
-						break;
-					case 'scrollTo':
-						// driver.findElement(By.css(cmds[i].params[0])).then(function(el) {
-						// 	el.getLocation().then(function (position) {
-						// 		driver.touchActions().scroll({x:position.x,y:position.y}).perform();
-						// 	})
-						// 	});
-						break;
-					case 'getText':
-						// var text = driver.findElement(By.css(cmds[i].params[0])).getText();
-						break;
-					case 'fillIn':
-						guideAutomator.fillIn(cmds[i].params[0], cmds[i].params[1]);
-						break;
-					case 'submit':
-						guideAutomator.submit(cmds[i].params[0]);
-						break;
-					case 'click':
-						guideAutomator.click(cmds[i].params[0]);
-						break;
-					case 'clickByLinkText':
-						guideAutomator.clickByLinkText(cmds[i].params[0]);
-						break;
-					case 'hover':
-						// driver.findElement(By.css(cmds[i].params[0])).then(function(elem){
-						// 	driver.actions().mouseMove(elem).perform();
-						// 	driver.sleep(cmds[i].params[1]);
-						// });
-						break;
-					case 'sleep':
-						guideAutomator.sleep(cmds[i].params[0]);
-						break;
-					case 'wait':
-						guideAutomator.wait(cmds[i].params[0], cmds[i].params[1]);
-						break;
-					default:
-						break;
-				}
-			}
-		}
-		return cb(null);
-	});
-}
 
 function replaceRemainingBlocks(cb) {
 	markdownText = markdownText.replace(/<replaceSelenium>/g, "").replace(/\\pagebreak/g, html_pagebreak);
@@ -162,38 +30,26 @@ function replaceRemainingBlocks(cb) {
 
 function guideAutomatorParser(mdText, cb) {
 	guideAutomator.defineOptions(options);
-	if (options.legacy) {
-		return extractMarkdownAndSelenium(mdText, function(seleniumBlocks) { //Extração dos blocos 'automator'
-			return execSelenium(seleniumBlocks, function(err) { //Execução e tratamento dos tokens dos blocos 'automator'
-				if (err) {
-					return cb(null, err);
-				} else {
-					guideAutomator.quit().then(function() {
-						return replaceRemainingBlocks(cb);
-					});
-				}
-			});
-		});
-	} else {
-		return extractJavascript(mdText, function(err, javascriptBlocks) {
-			if (err)
+
+	return extractJavascript(mdText, function(err, javascriptBlocks) {
+		if(err)
+			throw err;
+		return executeJavascriptSelenium(javascriptBlocks, function(err) {
+			if(err)
 				throw err;
-			return executeJavascriptSelenium(javascriptBlocks, function(err) {
-				if (err)
-					throw err;
 
-				guideAutomator.quit().then(function() {
-					if (options.debug) {
-						console.log("");
-						console.timeEnd('Selenium');
-					}
+			guideAutomator.quit().then(function() {
+				if(options.debug) {
+					console.log("");
+					console.timeEnd('Selenium');
+				}
 
-					return replaceRemainingBlocks(cb);
-				});
-
+				return replaceRemainingBlocks(cb);
 			});
+
 		});
-	}
+	});
+
 }
 //-- Fim Tratamento dos tokens ou execução de funcionalidades 'automator' ------
 //--------------------------------------------------------------------
@@ -202,7 +58,7 @@ function replaceBlockWithJsStdout(blockIndex, jsStdout) {
 	var index = 0;
 	markdownText = markdownText.replace(/<replaceSelenium>/g, function(match) {
 		index++;
-		if ((index - 1) === blockIndex) {
+		if((index - 1) === blockIndex) {
 			return '<replaceSelenium>' + jsStdout;
 		}
 		return match;
@@ -219,13 +75,13 @@ function extractJavascript(markdownAndCode, cb) {
 }
 
 function executeJavascriptSelenium(markdownText, cb) {
-	if (options.debug) {
+	if(options.debug) {
 		console.log(`File's JSBlocks: ` + seleniumBlocks.length);
 		console.log("");
 		console.time('Selenium');
 	}
 
-	for (var n_block = 0; n_block < seleniumBlocks.length; n_block++) {
+	for(var n_block = 0; n_block < seleniumBlocks.length; n_block++) {
 		guideAutomator.executeExternFunction(seleniumBlocks[n_block]);
 		var jsStdout = guideAutomator.getReturn();
 		replaceBlockWithJsStdout(n_block, jsStdout);
